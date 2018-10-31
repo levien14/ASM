50bfb88d818e99258fdea9079167830d59d1c935
LeVien <vienlvv14091998@gmail.com>
2018-10-31 22:39:33 +0700

    first commit

---
+++ MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MyNetCoreMVC.Models;
+
+namespace MyNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Products.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Save(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            TempData["Create"] = "Create Success";
+            return Redirect("Index");
+        }
+
+        public IActionResult Edit(long id)
+        {
+            var checke = _context.Products.Find(id);
+            if (checke == null)
+            {
+                return NotFound();
+            }
+            return View(checke);
+        }
+
+        public IActionResult Update(Product product)
+        {
+            var checke = _context.Products.Find(product.id);
+            if (checke == null)
+            {
+                return NotFound();
+            }
+
+            checke.name = product.name;
+            checke.price = product.price;
+            _context.Products.Update(checke);
+            _context.SaveChanges();
+            return Redirect("Index");
+        }
+
+        public IActionResult Delete(Product product)
+        {
+            var del = _context.Products.Find(product.id);
+            if (del == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(del);
+            _context.SaveChanges();
+            return Redirect("Index");
+
+        }
+    }
+}
---
+++ MyNetCoreMVC/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $(".btnDelete").click(function () {
+        var currentBtn = $(this);
+        if (confirm("Do you want to delete this Product")) {
+            $.ajax({
+                url: "/Product/Delete?id=" + currentBtn.attr("id"),
+                method: "DELETE",
+                success: function () {
+                    $(".alert-success").text("Delete Success");
+                    currentBtn.closest("li").remove();
+                },
+                error: function () {
+                    $(".alert-danger").text("Delete Faid");
+                }
+            });
+
+        };
+    });
+})
